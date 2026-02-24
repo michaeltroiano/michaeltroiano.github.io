57841916a7977c43ad272ae0ca708c4b8d2b3559
Michael Troiano <troiano.m93@gmail.com>
2026-02-24 11:47:18 -0500

    Ignore files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import './styles/global.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import './styles/global.css'
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div className='app'>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/services'
+            element={<Services />}
+          />
+          <Route
+            path='/contact'
+            element={<Contact />}
+          />
         </Routes>
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
